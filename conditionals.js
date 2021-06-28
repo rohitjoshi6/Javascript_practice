ce5c0ceb2bbd3600801260b07fd03937bcb362f2
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-06-28 20:00:37 +0530

    AND , conditionals

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+function trueorfalse(digit){
+    if (digit==5){
+        return "True";
+    }
+    else{
+        return "False"
+    }
+}
+
+console.log(trueorfalse(5))
---
+++ logicalAND.js
@@ -0,0 +1,8 @@
+function logic(num){
+    if(num>=100 && num<=120){
+        return "Correct";
+    }
+    return "Incorrect";
+}
+
+console.log(logic(121));
