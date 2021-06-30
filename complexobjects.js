1e05f7f3f0042ae0cd37492ce6b89c16fc58be35
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-06-30 23:39:49 +0530

    complex objects

---
+++ complexobjects.js
@@ -0,0 +1,18 @@
+var compobject={
+    car:{
+        inside:{
+            model:"s8",
+            year:2019,
+            company:"Tesla"
+        },
+        outside:{
+            color:"Red",
+            mirror:2,
+            tyres:4
+        }
+    }
+}
+
+
+var ans=compobject.car.inside.year
+console.log(ans)
