c2c90413a1d7c622ea4582d21de5900088f08e1b
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-07-02 23:09:01 +0530

    for,additionofarray

---
+++ additionofarray.js
@@ -0,0 +1,8 @@
+var myarr=[10,20,30];
+var finans=0;
+
+for(i=0;i<myarr.length;i++){
+    finans+=myarr[i];
+}
+
+console.log(finans)
---
+++ for.js
@@ -0,0 +1,7 @@
+var answer=[]
+
+for(i=0;i<=10;i++){
+    answer.push(i)
+}
+
+console.log(answer)
